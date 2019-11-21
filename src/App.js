1d0556df0729c50cb118eea12fcd903822ad8281
eposha <freedoctime@gmail.com>
2019-11-21 13:53:47 +0300

    test branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import './App.scss';
-import MainScreen from './screens/MainScreen';
 
-function App() {
-    return (
-        <div className="App">
-            <MainScreen />
-        </div>
-    );
-}
-
-export default App;
